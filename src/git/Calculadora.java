b17866c057dfdac1a94f0b4d0949d530174a76e9
YagoQueHago <yurrutiavila@gmail.com>
2022-06-30 16:47:12 +0200

    Calculadora Granada1

--- src/git/Calculadora.java
+++ src/git/Calculadora.java
@@ -1,6 +1,21 @@
+/************ CALCULADORA (LAYOUTS) *****************************
+ * 
+ * PRIMER PASO:
+ * crear marco y lámina. 
+ * crear Layout display y botones con dos láminas
+ * El display irá en disposición "BorderLayout.NORTH"
+ * El teclado irá en una segunda lámina en la zona CENTER en disposición "GridLayout"
+ * Poner los botones y utilizar el mismo método para ponerlos a la escucha
+ * Omitir, de momento, los botones de operaciones y conseguir que los botones
+ *  se pongan a continuación el uno del otro
+ * 
+ ***************************************************************/  
+
 package git;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -39,88 +54,166 @@ class MarcoCalculadora extends JFrame{
 }//CIERRE CLASE MARCO
 
 
-class LaminaCalculadora extends JPanel{//lamina donde estará el display
+class LaminaCalculadora extends JPanel { 
 	
 	public LaminaCalculadora() {
 		
 		
 		/** CREAMOS UNA LAMINA PARA EL DISPLAY **/ 
 		
-		setLayout(new BorderLayout());
+		principio=true; // variable iniciada fuera del constructor (para eliminación del 0 en el display)
 		
-		JButton pantalla=new JButton("0");//este será el display
+		/** Instanciamos la disposición dentro de los argumentos del método (forma abreviada) **/
 		
-		pantalla.setEnabled(false);//inabilitamos botón
+		setLayout(new BorderLayout()); //"setLayout" establece o determina la disposición (método de la clase "Container").
 		
-		add(pantalla, BorderLayout.NORTH);
+		display=new JButton("0"); //este será el display (variable iniciada en el constructor, declarada fuera tb) 
+		
+		display.setEnabled(false); //inhabilitamos botón para que no se pueda presionar
+		
+		add(display, BorderLayout.NORTH); // el segundo argumento es una constante de clase de tipo INT
+		
+		// ("add" pertenece a la clase "Container" ; JPanel y JFrame heredan de "Container").
 		
 		
 		/** CREAMOS LA SEGUNDA LÁMINA PARA EL TECLADO **/
 		
-		milamina2=new JPanel(); //la iniciamos dentro del constructor, pero la declaramos fuera, para tener acceso a ella desde el método
+		teclado=new JPanel(); //la iniciamos dentro del constructor, pero la declaramos fuera, para tener acceso a ella desde el método que añade los botones y queda a la escucha.
 		
-		milamina2.setLayout(new GridLayout(4,4));//lo ordena por filas (1,2,3,4...) no por columnas.
+		teclado.setLayout(new GridLayout(4,4)); //lo ordena por filas, no por columnas.
+		
+		// ("setLayout" pertenece a la clase "Container" ; JPanel y JFrame heredan de "Container").
 		
 		
 		/** NOTA 1:
 		 * 
-		 * 		Creamos un método, fuera del constructor, que añade los botones.
+		 * 	Creamos un método, fuera del constructor, que añade los botones. (está más abajo)
+		 * 
+		 *  Utilizaremos este mismo método para poner los números a la escucha y que se cree un 
+		 *  
+		 *  evento ActionEvent que se almacene en la variable "e" de dicho evento.
+		 *  
+		 *  (por eso debemos pasar dos parámetros al método 'ponerBoton')
 		 * 
 		 */
 		
-		ponerBoton("7");
+		ActionListener insertar=new InsertaNumero();
 		
-		ponerBoton("8");
+		ponerBoton("7", insertar);
 		
-		ponerBoton("9");
+		ponerBoton("8", insertar);
 		
-		ponerBoton("/");
+		ponerBoton("9", insertar);
 		
-		ponerBoton("4");
+		ponerBoton("/", null);
 		
-		ponerBoton("5");
+		ponerBoton("4", insertar);
 		
-		ponerBoton("6");
+		ponerBoton("5", insertar);
 		
-		ponerBoton("*");
+		ponerBoton("6", insertar);
 		
-		ponerBoton("1");
+		ponerBoton("*", null);
 		
-		ponerBoton("2");
+		ponerBoton("1", insertar);
 		
-		ponerBoton("3");
+		ponerBoton("2", insertar);
 		
-		ponerBoton("-");
+		ponerBoton("3", insertar);
 		
-		ponerBoton("0");
+		ponerBoton("-", null);
 		
-		ponerBoton(".");
+		ponerBoton("0", insertar);
 		
-		ponerBoton("-");
+		ponerBoton(".", insertar);
 		
-		ponerBoton("+");
+		ponerBoton("=", null);
+		
+		ponerBoton("+", null);
 		
 		
 		/** NOTA 2:
 		 * 
-  		 * 		Añadimos la lámina "2" (de tipo grill) en la zona centro (tipo border)
+  		 * 		Añadimos la lámina "teclado" (tipo grill) en la zona "CENTER" (tipo border)
 		 * 
 		 */
 
 		
-		add(milamina2, BorderLayout.CENTER);
+		add(teclado, BorderLayout.CENTER);
 		
 	}
 	
-	private void ponerBoton(String caracter) {
+	/** NOTA 3:
+	 * 
+	 * Utilizaremos este método no solo para añadir los botones sino 
+	 * 
+	 * que además nos servirá para ponerlos a la escucha con 'ActionListener'
+	 * 
+	 */
+	
+	private void ponerBoton(String caracter, ActionListener oyente) {
 		
-		JButton boton=new JButton(caracter);
+		JButton boton=new JButton(caracter); // añade botón
 		
-		milamina2.add(boton);
+		boton.addActionListener(oyente); // ponemos el botón a la escucha y desencadena el evento 'actionPerformed' cuando hacemos click. 
+		
+		teclado.add(boton);
 	}
 	
-	private JPanel milamina2; // (declarada fuera del constructor, iniciada en el constructor)
+	/** NOTA 4
+	 * 
+	 * 1º PONER A LA ESCUCHA TODOS LOS BOTONES
+	 * 
+	 * 2º CAPTURAR EL TEXTO PARA QUE APAREZCA EN EL DISPLAY
+	 *
+	 */
+	
+	private class InsertaNumero implements ActionListener{ //interface, debe implementar método 'actionPerformed'
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			// 1º almacenamos aquí el texto del botón pulsado.
+			
+			String entrada=e.getActionCommand(); 
+			
+			// IF para que no aparezca 0 por defecto en el display
+			
+			if (principio) {
+				
+				display.setText("");
+				
+				principio=false;
+			}
+			
+			
+			
+			/** NOTA (método)
+			 * 
+			 *  2º hacemos aparecer en el display el texto almacenado.(variable entrada)
+			 *  y para que se vayan poniendo en linea todos los numeros utilizamos el método 'getText' (lo que hay en el display) 
+			 *  de lo contrario solo sale un número en el display.
+			 *  
+			 */
+			
+			display.setText(display.getText() + entrada); 
+			
+		}
 		
+		
+	}
+	
+	/** NOTA 5:
+	 * 
+	 * Variables declaradas, pero iniciadas en el constructor (se necesitan aquí para acceso por fuera)
+	 *  
+	 */
+	
+	private JPanel teclado; 
+		
+	private JButton display; 
+	
+	private boolean principio; //variable para que no salga "0" al principio
+	
 	
 }//CIERRE CLASE LÁMINA
 
